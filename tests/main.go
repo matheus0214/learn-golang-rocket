97febc9fd70a675589a036e2367fbe6f40741689
Matheus G. Dias <matheusgiga123@gmail.com>
2025-02-10 21:23:23 -0300

    create a folder to do tests

---
+++ tests/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var moviesInDB = []string{
+	"Movie 12",
+	"Movie 13",
+	"Movie 14",
+	"Movie 15",
+	"Movie 16",
+	"Movie 17",
+	"Movie 18",
+	"Movie 19",
+	"Movie 20",
+	"Movie 21",
+	"Movie 22",
+}
+
+func main() {
+	boundsCheck([]int{1, 2, 3, 4})
+}
+
+func boundsCheck(items []int) {
+	_ = items[3]
+	fmt.Println(items[0])
+	fmt.Println(items[1])
+	fmt.Println(items[2])
+	fmt.Println(items[3])
+}
+
+func performaceInSlices() {
+	resultsFromApi := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	movies := make([]string, 0, 10)
+
+	for _, id := range resultsFromApi {
+		movie := moviesInDB[id]
+		fmt.Println(len(movies), cap(movies))
+		movies = append(movies, movie)
+	}
+
+	fmt.Println(len(movies), cap(movies))
+	fmt.Println(movies)
+}
