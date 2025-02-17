df607b922b2401d796d3ef7c87f74a982544f624
Matheus G. Dias <matheusgiga123@gmail.com>
2025-02-17 11:22:59 -0300

    using in memory db

--- workingWithDB/sqlite/sqlite.go
+++ workingWithDB/sqlite/sqlite.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
+
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitex"
 )
 
 func ConnectSqlite() {
@@ -52,4 +56,23 @@ func ConnectSqlite() {
 	}
 
 	fmt.Println(u)
+}
+
+func InMemory() {
+	conn, err := sqlite.OpenConn(":memory:", sqlite.OpenReadWrite)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	err = sqlitex.ExecuteTransient(conn, "SELECT 'hello, world';", &sqlitex.ExecOptions{
+		ResultFunc: func(stmt *sqlite.Stmt) error {
+			fmt.Println(stmt.ColumnText(0))
+			return nil
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
 }
