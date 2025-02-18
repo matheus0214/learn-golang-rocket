e87134213175c8d342b9dff454d1b718fde87389
Matheus G. Dias <matheusgiga123@gmail.com>
2025-02-18 11:15:42 -0300

    define user routes

---
+++ gobid/internal/api/user_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (a *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+	panic("Need implementation")
+}
+
+func (a *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("Need implementation")
+}
+
+func (a *Api) handleLogoutUser(w http.ResponseWriter, r *http.Request) {
+	panic("Need implementation")
+}
